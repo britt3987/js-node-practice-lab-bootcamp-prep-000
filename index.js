fdbf71f16addfc226688b2e17cdd9eddab3a2a4c
brittany <brittiepie322@aim.com>
2018-03-17 20:20:30 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,14 +1,15 @@
 // write your code below!
 
 var name = "Brittany"
-var height = "62"
+var height = 62
 
 console.log("Name:")
 console.log(name)
 
 console.log("Height:")
 console.log(height)
-console.log(parseInt(height) + 1)
+console.log(height + 1)
+
 
 
 
